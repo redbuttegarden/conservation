463926ad67e14aba184f87a9230b43b916436cd7
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2018-12-03 11:10:58 -0700

    Training Script for VGGNet
    Fleshed out existing training script for the VGG19 network.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- rana_dl/train_vggnet.py
+++ rana_dl/train_vggnet.py
@@ -11,27 +11,20 @@ import argparse
 import json
 import logging
 import os
-import random
 
-from imutils import paths
 import mxnet as mx
 
-from rana_dl.models.networks.mxvggnet import MxVGGNet
+from .models.networks.mxvggnet import MxVGGNet
+from .build_mx_dataset import DATASET_MEAN, MX_OUTPUT
 
 NUM_CLASSES = 2
-MX_OUTPUT = os.path.expanduser("~/data")
-TRAIN_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/train.lst"])
-VAL_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/val.lst"])
-TEST_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/test.lst"])
 
 TRAIN_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/train.rec"])
 VAL_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/val.rec"])
 TEST_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/test.rec"])
 
-DATASET_MEAN = "output/pollinator_mean.json"
-
 BATCH_SIZE = 64
-NUM_DEVICES = 8
+NUM_DEVICES = 1
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--checkpoints", required=True,
@@ -46,46 +39,88 @@ logging.basicConfig(level=logging.DEBUG,
                     filename="training_{}.log".format(args["start_epoch"]),
                     filemode="w")
 
-# initialize the data and labels
-print("[INFO] loading images...")
-data = []
-labels = []
+# Load the RGB means for the training set, then determine the batch
+# size
+means = json.loads(open(DATASET_MEAN).read())
+bat_size = BATCH_SIZE * NUM_DEVICES
 
-# grab the image paths and randomly shuffle them
-image_paths = sorted(list(paths.list_images(args["dataset"])))
-random.seed(42)
-random.shuffle(image_paths)
+train_iter = mx.io.ImageRecordIter(
+    path_imgrec=TRAIN_MX_REC,
+    data_shape=(3, 84, 84),
+    batch_size=bat_size,
+    rand_crop=True,
+    rand_mirror=True,
+    rotate=15,
+    max_shear_ratio=0.1,
+    mean_r=means["R"],
+    mean_g=means["G"],
+    mean_b=means["B"],
+    preprocess_threads=NUM_DEVICES * 2
+)
 
-# Loop over the input images
-for img_path in image_paths:
-    # Load the image, pre-process it, and store it in the data list
-    image = cv2.imread(img_path)
-    image = cv2.resize(image, (84, 84))
-    image = img_to_array(image)
-    data.append(image)
+val_iter = mx.io.ImageRecordIter(
+    path_imgrec=VAL_MX_REC,
+    data_shape=(3, 84, 84),
+    batch_size=bat_size,
+    mean_r=means["R"],
+    mean_g=means["G"],
+    mean_b=means["B"]
+)
 
-    # Extract the class label from the image path and update the
-    # labels list
-    label = img_path.split(os.path.sep)[-2]
-    label = 1 if label == "Pollinator" else 0
-    labels.append(label)
+# Initialize the optimizer
+opt = mx.optimizer.SGD(learning_rate=1e-2, momentum=0.9, wd=0.0005,
+                       rescale_grad=1.0 / bat_size)
 
-"""
-At this point labels are imbalanced so we want to figure out the
-most frequent label and remove data with that label until both
-label classes are evenly represented.
-"""
-c = Counter(labels)
-label_freqs = c.most_common(2)
-over_represented, or_freq = label_freqs[0]
-under_represented, ur_freq = label_freqs[1]
+# Construct the checkpoints path, initialize the model argument and
+# auxillary parameters
+checkpoints_path = os.path.sep.join([args["checkpoints"],
+                                     args["prefix"]])
+arg_params = None
+aux_params = None
 
-# Now that we have the label frequencies, we purge the
-# over-represented data
-paired = zip(labels, data)
-sorted_paired = sorted(paired, key=lambda x: x[0])  # Sort by the label
-# Unzip a subset of the data based on the difference between labels
-labels, data = zip(*sorted_paired[or_freq - ur_freq:])
+# If there is no specific model starting epoch supplied, then
+# initialize the network
+if args["start_epoch"] <= 0:
+    # Build the VGGNet architecture
+    print("[INFO] Building network...")
+    model = MxVGGNet.build(NUM_CLASSES)
 
-data = np.array(data, dtype="float") / 255.0
-labels = np.array(labels)
+# Otherwise, a specific checkpoint was supplied
+else:
+    # Load the checkpoint from disk
+    print("[INFO] Loading epoch {}...".format(args["start_epoch"]))
+    model = mx.model.FeedForward.load(checkpoints_path,
+                                      args["start_epoch"])
+
+    # Update the model and parameters
+    arg_params = model.arg_params
+    aux_params = model.aux_params
+    model = model.symbol
+
+# Compile the model
+model = mx.model.FeedForward(
+    ctx=[mx.gpu(i) for i in range(0, NUM_DEVICES)],
+    symbol=model,
+    initializer=mx.initializer.MSRAPrelu(),
+    arg_params=arg_params,
+    aux_params=aux_params,
+    optimizer=opt,
+    num_epoch=80,
+    begin_epoch=args["start_epoch"]
+)
+
+# Initialize the callbacks and evaluation metrics
+batch_end_CBs = [mx.callback.Speedometer(bat_size, 250)]
+epoch_end_CBs = [mx.callback.do_checkpoint(checkpoints_path)]
+metrics = [mx.metric.Accuracy(), mx.metric.TopKAccuracy(top_k=5),
+           mx.metric.CrossEntropy()]
+
+# Train the network
+print("[INFO] Training network...")
+model.fit(
+    X=train_iter,
+    eval_data=val_iter,
+    eval_metric=metrics,
+    batch_end_callback=batch_end_CBs,
+    epoch_end_callback=epoch_end_CBs
+)
