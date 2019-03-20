55480473367c6767896a6af230546fdf57b4cd0d
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2019-03-20 14:03:56 -0600

    Updated config to save to network location with fewer disk constraints.
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- rana_dl/models/config.py
+++ rana_dl/models/config.py
@@ -5,7 +5,7 @@ BATCH_SIZE = 4
 NUM_TEST_IMAGES = 100 * NUM_CLASSES
 NUM_VAL_IMAGES = int(100 * NUM_CLASSES / 2)
 
-MX_OUTPUT = os.path.expanduser("~/data")
+MX_OUTPUT = os.path.expanduser("/scratch/general/lustre/u6000791")
 TRAIN_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/train.lst"])
 VAL_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/val.lst"])
 TEST_MX_LIST = os.path.sep.join([MX_OUTPUT, "lists/test.lst"])
@@ -14,6 +14,6 @@ TRAIN_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/train.rec"])
 VAL_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/val.rec"])
 TEST_MX_REC = os.path.sep.join([MX_OUTPUT, "rec/test.rec"])
 
-DATASET_MEAN = os.path.expanduser("~/data/output/vggnet/pollinator_mean.json")
+DATASET_MEAN = "/scratch/general/lustre/u6000791/rec/pollinator_mean.json"
 
 LOG_OUTPUT = os.path.expanduser("~/data/output")
