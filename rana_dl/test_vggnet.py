72ed2d02d951fc6bc0deda18ed7ea055cf75ee79
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2019-04-01 11:47:52 -0600

    Flexibility, API Updates, And Eval Metric Additions
    Dataset averages file as well as their associated test record files are
    now supplied to the test script as commandline arguments.
    
    Model loading now uses the updated Module.load function since the
    previously used FeedForward method is deprecated.
    
    The F1 metric was added to the model evaluations as it also provides
    access to a record of the number of false positives and negatives when
    testing binary classifiers.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- rana_dl/test_vggnet.py
+++ rana_dl/test_vggnet.py
@@ -9,18 +9,22 @@ from models import config
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--checkpoints", required=True,
                 help="path to output checkpoint directory")
+ap.add_argument("-m", "--means", required=True,
+                help="path to image channels means file associated with training data")
 ap.add_argument("-p", "--prefix", required=True,
                 help="name of model prefix")
+ap.add_argument("-r", "--record", required=True,
+                help="path to record file to be used for testing")
 ap.add_argument("-e", "--epoch", type=int, required=True,
                 help="epoch number to load")
 args = vars(ap.parse_args())
 
 # Load the RGB means for the training set
-means = json.loads(open(config.DATASET_MEAN).read())
+means = json.loads(open(args["means"]).read())
 
 # Construct the testing image iterator
 test_iter = mx.io.ImageRecordIter(
-    path_imgrec=config.TEST_MX_REC,
+    path_imgrec=args["record"],
     data_shape=(3, 480, 640),
     batch_size=config.BATCH_SIZE,
     mean_r=means["R"],
@@ -32,21 +36,27 @@ test_iter = mx.io.ImageRecordIter(
 print("[INFO] Loading model...")
 checkpoints_path = os.path.sep.join([args["checkpoints"],
                                      args["prefix"]])
-model = mx.model.FeedForward.load(checkpoints_path,
-                                  args["epoch"])
+model = mx.module.Module.load(checkpoints_path, args["epoch"])
+
 
 # Compile the model
-model = mx.model.FeedForward(
-    ctx=[mx.cpu()],
-    symbol=model.symbol,
-    arg_params=model.arg_params,
-    aux_params=model.aux_params
-)
+model.bind(data_shapes=test_iter.provide_data,
+           label_shapes=test_iter.provide_label)
 
 # Make predictions on the testing data
 print("[INFO] Predicting on test data...")
-metrics = [mx.metric.Accuracy()]
-rank1 = model.score(test_iter, eval_metric=metrics)[0]
+metrics = [mx.metric.Accuracy(), mx.metric.F1(average="micro")]
+rank1 = model.score(test_iter, eval_metric=metrics)[0][1]
 
 # Display the rank-1 accuracies
-print("[INFO] rank-1: {:.2f}%".format(rank1 * 100))
+print("[INFO] rank-1 Accuracy: {:.2f}%".format(rank1 * 100))
+
+# The F1 metric inherits from a class that tracks the number
+# of false positives and negatives, allowing us to extract them
+metric = metrics[1]
+print("False Positives: {}\n" 
+      "False Negatives: {}\n" 
+      "True Positives: {}\n" 
+      "True Negatives: {}\n"
+      .format(metric.metrics.false_positives, metric.metrics.false_negatives,
+              metric.metrics.true_positives, metric.metrics.true_negatives))
