95f9059e5cd06f41921d35e01c5d79fc7e653493
Avery Uslaner <avery.uslaner@redbutte.utah.edu>
2019-04-02 09:51:06 -0600

    Model Training Gluon API Conversion
    Converted training script to using newly defined Gluon API model
    architecture.
    
    Also added commented out channel average file path as a commandline
    argument since future iterations will hopefully make use of it.
    
    Default arguments were modified for learning rate and ending epoch to
    1e-3 and 100 respectively.
    
    Signed-off-by: Avery Uslaner <avery.uslaner@redbutte.utah.edu>

--- rana_dl/train_vggnet.py
+++ rana_dl/train_vggnet.py
@@ -13,22 +13,27 @@ import logging
 import os
 
 import mxnet as mx
+from mxnet import gluon
+from mxnet.gluon.data import DataLoader
+from mxnet.gluon.data.vision.datasets import ImageRecordDataset
 
 from models import config
-from models.networks.mxvggnet import MxVGGNet
+from models.networks.mxvggnet import VGG19
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--checkpoints", required=True,
                 help="path to output checkpoint directory")
-ap.add_argument("-lr", "--learning-rate", type=float, default=1e-2,
+ap.add_argument("-lr", "--learning-rate", type=float, default=1e-3,
                 help="learning rate to use for training")
 ap.add_argument("-n", "--num-devices", type=int, default=1,
                 help="number of GPUs to run on")
 ap.add_argument("-p", "--prefix", required=True,
                 help="name of model prefix")
+# ap.add_argument("-m", "--means", required=True,
+#                 help="path to image channels means file associated with training data")
 ap.add_argument("-s", "--start-epoch", type=int, default=0,
                 help="epoch to restart training at")
-ap.add_argument("-e", "--end-epoch", type=int, default=80,
+ap.add_argument("-e", "--end-epoch", type=int, default=100,
                 help="epoch to end training at")
 args = vars(ap.parse_args())
 
@@ -39,85 +44,90 @@ logging.basicConfig(level=logging.DEBUG,
 
 # Load the RGB means for the training set, then determine the batch
 # size
-means = json.loads(open(config.DATASET_MEAN).read())
+# means = json.loads(open(args["means"]).read())
 bat_size = config.BATCH_SIZE * args["num_devices"]
 
-train_iter = mx.io.ImageRecordIter(
-    path_imgrec=config.TRAIN_MX_REC,
-    data_shape=(3, 480, 640),
-    batch_size=bat_size,
-    #rand_crop=True,
-    rand_mirror=True,
-    #rotate=15,
-    #max_shear_ratio=0.1,
-    mean_r=means["R"],
-    mean_g=means["G"],
-    mean_b=means["B"],
-    preprocess_threads=args["num_devices"] * 2
-)
+train_dataset = ImageRecordDataset(config.TRAIN_MX_REC)
+transformer = gluon.data.vision.transforms.ToTensor()
+train_dataset = train_dataset.transform_first(transformer)
 
-val_iter = mx.io.ImageRecordIter(
-    path_imgrec=config.VAL_MX_REC,
-    data_shape=(3, 480, 640),
-    batch_size=bat_size,
-    mean_r=means["R"],
-    mean_g=means["G"],
-    mean_b=means["B"]
-)
+val_dataset = ImageRecordDataset(config.VAL_MX_REC)
+val_dataset = val_dataset.transform_first(transformer)
 
-# Initialize the optimizer
-opt = mx.optimizer.SGD(learning_rate=args["learning_rate"], momentum=0.9, wd=0.0005,
-                       rescale_grad=1.0 / bat_size)
+train_dataloader = DataLoader(train_dataset, batch_size=bat_size, shuffle=True,
+                              num_workers=4)
+val_dataloader = DataLoader(val_dataset, batch_size=bat_size, shuffle=False,
+                            num_workers=4)
 
 # Construct the checkpoints path, initialize the model argument and
 # auxillary parameters
 checkpoints_path = os.path.sep.join([args["checkpoints"],
                                      args["prefix"]])
-arg_params = None
-aux_params = None
 
 # If there is no specific model starting epoch supplied, then
 # initialize the network
 if args["start_epoch"] <= 0:
     # Build the VGGNet architecture
     print("[INFO] Building network...")
-    model = MxVGGNet.build(config.NUM_CLASSES)
+    model = VGG19()
 
 # Otherwise, a specific checkpoint was supplied
-else:
-    # Load the checkpoint from disk
-    print("[INFO] Loading epoch {}...".format(args["start_epoch"]))
-    model = mx.model.FeedForward.load(checkpoints_path,
-                                      args["start_epoch"])
+# else:
+#     # Load the checkpoint from disk
+#     print("[INFO] Loading epoch {}...".format(args["start_epoch"]))
+#     model = mx.module.Module.load(checkpoints_path, 100)
+#     model.bind(data_shapes=train_dataloader.provide_data, label_shapes=test_iter.provide_label)
 
-    # Update the model and parameters
-    arg_params = model.arg_params
-    aux_params = model.aux_params
-    model = model.symbol
+ctx = [mx.gpu(i) for i in range(0, args["num_devices"])]
 
-# Compile the model
-model = mx.model.FeedForward(
-    ctx=[mx.gpu(i) for i in range(0, args["num_devices"])],
-    symbol=model,
-    initializer=mx.initializer.MSRAPrelu(),
-    arg_params=arg_params,
-    aux_params=aux_params,
-    optimizer=opt,
-    num_epoch=args["end_epoch"],
-    begin_epoch=args["start_epoch"]
-)
+model.initialize(mx.initializer.MSRAPrelu(), ctx=ctx)
+trainer = gluon.Trainer(model.collect_params(), "sgd", {"learning_date": args["learning_rate"]})
 
-# Initialize the callbacks and evaluation metrics
-batch_end_CBs = [mx.callback.Speedometer(bat_size, 50)]
-epoch_end_CBs = [mx.callback.do_checkpoint(checkpoints_path)]
-metrics = [mx.metric.Accuracy(), mx.metric.CrossEntropy()]
+# Initialize the evaluation metrics
+metrics = [mx.metric.Accuracy()]
+
+# Define our loss function
+loss_fn = gluon.loss.SoftmaxCrossEntropyLoss()
 
 # Train the network
 print("[INFO] Training network...")
-model.fit(
-    X=train_iter,
-    eval_data=val_iter,
-    eval_metric=metrics,
-    batch_end_callback=batch_end_CBs,
-    epoch_end_callback=epoch_end_CBs
-)
+for epoch in range(args["end_epoch"]):
+    # Training Loop
+    cumulative_train_loss = mx.nd.zeros(1, ctx=ctx)
+    training_samples = 0
+
+    for batch_idx, (data, label) in enumerate(train_dataloader):  # start of mini-batch
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+
+        with mx.autograd.record():
+            output = model(data)  # Forward pass
+            loss = loss_fn(output, label)  # Get loss
+
+        loss.backward()  # Compute gradients
+        trainer.step(data.shape[0])  # Update weights with SGD
+        cumulative_train_loss += loss.sum()
+        training_samples += data.shape[0]
+        metrics[0].update(label, output)  # Update the metrics # end of mini-batch
+
+    train_loss = cumulative_train_loss.asscalar() / training_samples
+
+    # Validation loop
+    cumulative_val_loss = mx.nd.zeros(1, ctx)
+    val_samples = 0
+    for batch_idx, (data, label) in enumerate(val_dataloader):
+        data = data.as_in_context(ctx)
+        label = label.as_in_context(ctx)
+
+        output = model(data)  # Forward pass
+        loss = loss_fn(output, label)  # Get loss
+
+        cumulative_val_loss += loss.sum()
+        val_samples += data.shape[0]
+
+    val_loss = cumulative_val_loss.asscalar() / val_samples
+
+    name, acc = metrics[0].get()
+    print("[Epoch {}] Training metrics: {}={}".format(epoch, name, acc))
+    print("[Epoch {}] Training loss: {:.2f}, validation loss: {:.2f}".format(epoch, train_loss, val_loss))
+    metrics[0].reset()  # End of epoch
